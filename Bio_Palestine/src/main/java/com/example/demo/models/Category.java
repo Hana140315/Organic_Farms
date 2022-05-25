4575e5bdeb6bf030ee918863c6d5d1a470abfab5
majed amria <majedamrya@hotmail.com>
2022-05-25 17:15:23 +0300

    first push java project

---
+++ Bio_Palestine/src/main/java/com/example/demo/models/Category.java
@@ -0,0 +1,79 @@
+package com.example.demo.models;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+
+@Entity
+@Table(name="categories")
+public class Category {
+
+	 	@Id
+	    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	    private Long id;
+	 	
+	 	@NotNull(message="Must be not null")
+	 	@Size(min=3,max=15,message = "Must be at least three charactar")
+	 	private String categoryTitle;
+	 	
+	 	@NotNull(message="Must be not null")
+	 	@Size(min=5,max=50,message = "Must be at least five charactar")
+	 	private String categoryDescription;
+	 	
+	 	@Column(updatable=false)
+	    @DateTimeFormat(pattern="yyyy-MM-dd")
+	    private Date createdAt;
+	    @DateTimeFormat(pattern="yyyy-MM-dd")
+	    private Date updatedAt;
+	    
+		public Category() {
+			
+		}
+
+		public Long getId() {
+			return id;
+		}
+
+		public void setId(Long id) {
+			this.id = id;
+		}
+
+		public String getCategoryTitle() {
+			return categoryTitle;
+		}
+
+		public void setCategoryTitle(String categoryTitle) {
+			this.categoryTitle = categoryTitle;
+		}
+
+		public String getCategoryDescription() {
+			return categoryDescription;
+		}
+
+		public void setCategoryDescription(String categoryDescription) {
+			this.categoryDescription = categoryDescription;
+		}
+	 	
+		@PrePersist
+	    protected void onCreate(){
+	        this.createdAt = new Date();
+	    }
+		
+	    @PreUpdate
+	    protected void onUpdate(){
+	        this.updatedAt = new Date();
+	    }
+	 
+}
+
---
+++ Bio_Palestine/src/main/java/com/example/demo/models/Farm.java
@@ -0,0 +1,161 @@
+package com.example.demo.models;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Entity
+@Table(name="farms")
+public class Farm {
+
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+	
+	@NotNull(message="Name must be not null")
+	@Size(min=3,max=10,message="Must be at least three character")
+	private String farmName;
+	
+	@NotNull(message="Address must be not null")
+	@Size(min=5,max=15,message="Must be at least five character")
+	private String farmAddress;
+	
+	@NotNull(message="Phone must be not null")
+	@Size(min=10, message="at least ten number")
+	private int farmPhone;
+	
+	@NotNull(message="Location must be not null")
+	@Size(min=5,message="at least five character")
+	private String farmLocation;
+	
+	@NotEmpty(message="Email is required!")
+    @Email(message="Please enter a valid email!")
+    private String email;
+	
+	@NotEmpty(message="Password is required!")
+    @Size(min=8, max=128, message="Password must be between 8 and 128 characters")
+    private String password;
+	
+	@Transient
+    @NotEmpty(message="Confirm Password is required!")
+    @Size(min=8, max=128, message="Confirm Password must be between 8 and 128 characters")
+    private String confirm;
+	
+	@Column(updatable=false)
+    @DateTimeFormat(pattern="yyyy-MM-dd")
+    private Date createdAt;
+    @DateTimeFormat(pattern="yyyy-MM-dd")
+    private Date updatedAt;
+    
+    public Farm() {
+	
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+
+	public String getFarmName() {
+		return farmName;
+	}
+
+
+
+	public void setFarmName(String farmName) {
+		this.farmName = farmName;
+	}
+
+
+
+	public String getFarmAddress() {
+		return farmAddress;
+	}
+
+
+
+	public void setFarmAddress(String farmAddress) {
+		this.farmAddress = farmAddress;
+	}
+
+
+
+	public int getFarmPhone() {
+		return farmPhone;
+	}
+
+
+
+	public void setFarmPhone(int farmPhone) {
+		this.farmPhone = farmPhone;
+	}
+
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+
+	public String getConfirm() {
+		return confirm;
+	}
+
+
+
+	public void setConfirm(String confirm) {
+		this.confirm = confirm;
+	}
+
+
+
+	@PrePersist
+    protected void onCreate(){
+        this.createdAt = new Date();
+    }
+	
+    @PreUpdate
+    protected void onUpdate(){
+        this.updatedAt = new Date();
+    }
+}
